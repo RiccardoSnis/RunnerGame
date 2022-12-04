9839e2e55d0b66c214672715c4fecdcf7e2f49ff
RiccardoSnis <riccardo.dipaola22@gmail.com>
2022-12-04 16:44:42 +0100

    added left right movement

--- Runner-game/Assets/scripts/player/PlayerMove.cs
+++ Runner-game/Assets/scripts/player/PlayerMove.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 3;
+    public float leftRightSpeed = 7;
 
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            if(this.gameObject.transform.position.x > LevelBoundery.leftSide)
+            transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+
+        }
+
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            if(this.gameObject.transform.position.x < LevelBoundery.rightSide)
+            transform.Translate(Vector3.right * Time.deltaTime * leftRightSpeed);
+        }
+
         
     }
 }
