34ad1933bfc31fe48e9805b5d0ff62d7f188f9c3
RiccardoSnis <riccardo.dipaola22@gmail.com>
2023-07-02 18:43:13 +0200

    rimozione sezioni superate

--- Runner-game/Assets/scripts/player/ObstacleCollision.cs
+++ Runner-game/Assets/scripts/player/ObstacleCollision.cs
@@ -73,7 +73,7 @@ public class ObstacleCollision : MonoBehaviour
             yield return null;
         }
 
-        levelControl.GetComponent<EndRunSequence>().enabled = true;
+       levelControl.GetComponent<EndRunSequence>().enabled = true;
     }
 
     private float FindGroundHeight()
